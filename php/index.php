e7b89859b7e32a403236b66bd37ee18dc152c1cd
Bazarovay <rxs6754@rit.edu>
2018-02-21 06:02:04 -0500

    button styling and javascript redirection

--- php/index.php
+++ php/index.php
@@ -3,9 +3,16 @@
  </head>
 <body>
 <head>
+
+
 <style type="text/css">
+
+body {
+    background-color: lightblue;
+}
+
 #header {
-    background: red;
+    background: #ff7f7f;
     height: 20%;
 }
 h1{
@@ -16,7 +23,7 @@ h1{
 }
 
 .button {
-    background-color: #4CAF50; /* Green */
+    background-color: orange;
     border: none;
     color: white;
     padding: 15px 32px;
@@ -25,7 +32,13 @@ h1{
     display: inline-block;
     font-size: 16px;
 }
+.button:hover {
+    background-color: red;
+    color: white;
+}
+
 </style>
+
 
 
 </head>
@@ -34,13 +47,10 @@ h1{
      <h1>Our Vulnerable PHP Webapp</h1>
     </div>
 
-<form name="form1" method="post" action="index.php">
+
 </tr>
-<tr class="tableheader">
-<td align="center" colspan="2">
-  <input class="button" type="submit" name="Home" value="Home"></td>
-</tr>
-</form>
+
+<button id="home" class="button" name="Home" value="Home"> |^| Home </button>
 
 
 <form name="form4" method="post" action="login.php">
@@ -68,6 +78,18 @@ h1{
   <input class="button" type="submit" name="lookup" value="Lookup ID"></td>
 </tr>
 </form>
+
+
+<script>
+
+var btn = document.getElementById('home');
+console.log(btn);
+if(btn){
+  btn.addEventListener('click', function() {
+    document.location.href = 'index.php';
+  });
+}
+</script>
 
 
 </body>
