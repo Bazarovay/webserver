87c50035a9bda64c42b38d75215a5084772b433b
Hans Johnson <hcj4880@rit.edu>
2018-02-20 17:07:51 -0800

    fixed issue where user couldn't log in

--- php/checklogin.php
+++ php/checklogin.php
@@ -16,7 +16,10 @@ echo "$password";
 $sql = "select * from users where username='$username' and password='$password'";
 $result = mysql_query($sql);
 $check = mysql_num_rows($result);
+echo intval($check);
 if ($check == 1){
+	session_start();
+	$_SESSION['username'] = $username;
 	header("Location: login-successful.php");
 } else {
 	header("Location: login.php");
