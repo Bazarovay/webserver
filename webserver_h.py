ac8f8b7650c0663d914dc74f17534cd98069e9fc
Hans Johnson <hcj4880@rit.edu>
2018-02-22 09:51:13 -0800

    updated webserver

--- webserver_h.py
+++ webserver_h.py
@@ -1,5 +1,5 @@
 import socket		#set up network connection
-import os		#for command-line execution of php-cgi
+import os.path		#for command-line execution of php-cgi
 import subprocess
 import threading	#to permit concurrent client requests
 import sys
@@ -10,11 +10,11 @@ methods = ""
 requests = ""
 bad_requests = ""
 ip_listen = ""
-port = ""
-#port = int(sys.argv[1])
+#port = ""
+port = int(sys.argv[1])
 
 #HOST --> HTTP header, HTTP_HOST --> ENV var for php-cgi
-http_bash_map=[["Host","HTTP_HOST"], ["Accept","HTTP_ACCEPT"],["Aceept-Language","HTTP_ACCEPT_LANGUAGE"],["Accept-Encoding","HTTP_ACCEPT_ENCODING"],["Connection","HTTP_Connection"]]
+http_bash_map=[["Host","HTTP_HOST"],["Content-Type","CONTENT_TYPE"],["Content-Length","CONTENT_LENGTH"],["Accept","HTTP_ACCEPT"],["Aceept-Language","HTTP_ACCEPT_LANGUAGE"],["Accept-Encoding","HTTP_ACCEPT_ENCODING"],["Connection","HTTP_Connection"]]
 
 #gets the requested file
 def getFile(http_req):
@@ -32,6 +32,10 @@ def getHTTPVersion(http_req):
 	splitreq = http_req.split("\n")
 	return splitreq[0].split(" ")[2]
 
+def getUser(http_req):
+	splitreq= http_req.split("\r\n\r\n")
+	return splitreq[1]
+
 def getHeaders(http_req):
 	exportList = ""
 	splitreq = http_req.split("\n")
@@ -40,9 +44,16 @@ def getHeaders(http_req):
 		headerparts = splitreq[headernum].split(":")
 		for map in http_bash_map:
 			if map[0] == headerparts[0]:
-				exportList +="export %s='%s';" % (map[1],headerparts[1].strip('\r'))
+				exportList +="export %s='%s';" % (map[1],headerparts[1][1:].strip('\r'))
 		headernum = headernum + 1
 	return exportList
+
+#def getLogin(http_req):
+#	data = ""
+#	splitreq = http_req.split("\n")
+#	if (splitreq[0].split(" ")[0] == "POST"):
+#		data = headerparts[1][1:]
+#	return data 
 
 def getVars(uri):
 	uriparts = uri.split("?")
@@ -56,23 +67,46 @@ def requestHandler(clientSock):
 	uriparts = getVars(getFile(request))
 	elist = getHeaders(request)
 
+	#if not (os.path.exists(getFile(request))):
+	#	response = "HTTP/1.1 404 HTTP Version not supported"
+	#	footer = "\r\n\r\n"
+	#	clientSock.send(response+footer)
+	#	clientSock.close()
+		
+		
+	#print("--",version,"--")
 
+	#version = version.strip("\r")
+	#print("--",version,"--")
 	#send back error 505 if version is not 1.1 or 1.2
-#	if (version == "HTTP/1.0") or (version == "HTTP/1.1") or (version == "HTTP/1.2"):
-#		response = "HTTP/1.1 505 HTTP Version not supported"
-#		footer = "\r\n\r\n"
-#		clientSock.send(response+footer)
-#		clientSock.close()
+	#if ((version != "HTTP/1.0") or (version != "HTTP/1.1") or (version != "HTTP/1.2")):
+	#if (version != "HTTP/1.0") and (version !="HTTP/1.1") and (version !="HTTP/1.2"):
+	#	response = "HTTP/1.1 505 HTTP Version not supported"
+	#	footer = "\r\n\r\n"
+	#	clientSock.send(response+footer)
+	#	clientSock.close()
+	#else:
 
 	
-	#Close the socket if Connection is not keep-alive
+		#Close the socket if Connection is not keep-alive
 
-	#Generate the php-cgi command
+		#Generate the php-cgi command
 	runscript = "%s export REQUEST_METHOD='%s'; " % (elist,method)
-	runscript = runscript + "export QUERY_STRING='%s'; " % (uriparts[1])
-	runscript = runscript + "export SCRIPT_FILENAME='%s%s'; " % (doc_root,uriparts[0])
-	runscript = runscript + " php-cgi -f %s%s" % (doc_root,uriparts[0])
-	
+	try:
+		runscript = runscript + "export QUERY_STRING='%s'; " % (uriparts[1])
+	except:
+		pass
+
+	if (method == "GET"):
+		runscript = runscript + "export SCRIPT_FILENAME='%s%s'; " % (doc_root,uriparts[0])
+		runscript = runscript + " php-cgi -f %s%s" % (doc_root,uriparts[0])
+	if (method == "POST"):
+		runscript = runscript + "export BODY='%s';" % (getUser(request))
+		runscript = runscript + "export SCRIPT_FILENAME='%s%s'; " % (doc_root,uriparts[0])
+		runscript = runscript + "echo $BODY | php-cgi "
+#	runscript = runscript + "export SCRIPT_FILENAME='%s%s'; " % (doc_root,uriparts[0])
+#	runscript = runscript + " php-cgi -f %s%s" % (doc_root,uriparts[0])
+	print("-",runscript,"-")	
 	response = "HTTP/1.1 200 Ok\r\n"
 	body = subprocess.check_output(runscript, shell=True)
 	footer = "\r\n\r\n"
@@ -99,7 +133,8 @@ def main():
 		config = [item.split(' ') for item in config]
 		for item in config:
 			if item[0] == "port":
-				port = int(item[1])
+				pass
+				#port = int(item[1])
 			if item[0] == "ip_listen":
 				ip_listen = item[1]
 			if item[0] == "methods":
